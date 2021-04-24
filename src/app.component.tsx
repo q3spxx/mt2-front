82a98aa23f2fb6fefc40cfd80d0e739160304f18
q3spxx <petrsokolkin@gmail.com>
2021-04-24 17:29:28 +0300

    check jenkins job 3

--- src/app.component.tsx
+++ src/app.component.tsx
@@ -8,7 +8,7 @@ import { appTheme } from './app.styles';
 export const theme = createMuiTheme(appTheme);
 
 const store = createStore();
-console.log(111);
+
 export const App = memo(() => (
     <Provider store={store}>
         <MuiThemeProvider theme={theme}>
